f607d72ede8961ad381ba55aa88d2b160f9cfdb3
lw <linhtm@cdmteck.com>
2024-07-17 16:57:49 +0700

    [UI] Update Auto lock type and Authenticate type selector

--- packages/extension-koni-ui/src/Popup/Settings/Security/index.tsx
+++ packages/extension-koni-ui/src/Popup/Settings/Security/index.tsx
@@ -1,16 +1,19 @@
 // Copyright 2019-2022 @subwallet/extension-koni-ui authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
+import { WalletUnlockType } from '@subwallet/extension-base/background/KoniTypes';
 import { Layout, PageWrapper } from '@subwallet/extension-koni-ui/components';
 import { EDIT_AUTO_LOCK_TIME_MODAL, EDIT_UNLOCK_TYPE_MODAL, settingsScreensLayoutBackgroundImages } from '@subwallet/extension-koni-ui/constants';
 import { DEFAULT_ROUTER_PATH } from '@subwallet/extension-koni-ui/constants/router';
 import useDefaultNavigate from '@subwallet/extension-koni-ui/hooks/router/useDefaultNavigate';
+import { saveAutoLockTime, saveUnlockType } from '@subwallet/extension-koni-ui/messaging';
 import { RootState } from '@subwallet/extension-koni-ui/stores';
 import { PhosphorIcon, ThemeProps } from '@subwallet/extension-koni-ui/types';
+import { noop } from '@subwallet/extension-koni-ui/utils';
 import { isNoAccount } from '@subwallet/extension-koni-ui/utils/account/account';
-import { BackgroundIcon, Icon, ModalContext, SettingItem, Switch } from '@subwallet/react-ui';
+import { BackgroundIcon, Icon, ModalContext, SettingItem, Switch, SwModal } from '@subwallet/react-ui';
 import CN from 'classnames';
-import { CaretRight, FingerprintSimple, Key } from 'phosphor-react';
+import { CaretRight, CheckCircle, FingerprintSimple, Key, LockKeyOpen, LockLaminated } from 'phosphor-react';
 import React, { useCallback, useContext, useMemo, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useSelector } from 'react-redux';
@@ -36,8 +39,15 @@ interface SecurityItem {
   disabled: boolean;
 }
 
+interface AutoLockOption {
+  label: string;
+  value: number;
+}
+
 const editAutoLockTimeModalId = EDIT_AUTO_LOCK_TIME_MODAL;
 const editUnlockTypeModalId = EDIT_UNLOCK_TYPE_MODAL;
+
+const timeOptions = [5, 10, 15, 30, 60];
 
 const Component: React.FC<Props> = (props: Props) => {
   const { className } = props;
@@ -48,13 +58,33 @@ const Component: React.FC<Props> = (props: Props) => {
   const location = useLocation();
   const canGoBack = !!location.state;
 
-  const { activeModal } = useContext(ModalContext);
+  const { activeModal, inactiveModal } = useContext(ModalContext);
 
   const { accounts } = useSelector((state: RootState) => state.accountState);
   const [isUseBiometric, setUseBiometric] = useState<boolean>(false);
   const [loadingBiometric, setLoadingBiometric] = useState(false);
+  const { timeAutoLock, unlockType } = useSelector((state: RootState) => state.settings);
 
   const noAccount = useMemo(() => isNoAccount(accounts), [accounts]);
+
+  const autoLockOptions = useMemo((): AutoLockOption[] => timeOptions.map((value) => {
+    if (value > 0) {
+      return {
+        value: value,
+        label: t('{{time}} minutes', { replace: { time: value } })
+      };
+    } else if (value < 0) {
+      return {
+        value: value,
+        label: t('Required once')
+      };
+    } else {
+      return {
+        value: value,
+        label: t('Always require')
+      };
+    }
+  }), [t]);
 
   const items = useMemo((): SecurityItem[] => [
     {
@@ -63,6 +93,20 @@ const Component: React.FC<Props> = (props: Props) => {
       title: t('Change wallet password'),
       url: '/keyring/change-password',
       disabled: noAccount
+    },
+    {
+      icon: LockLaminated,
+      key: SecurityType.AUTO_LOCK,
+      title: t('Auto lock'),
+      url: '',
+      disabled: false
+    },
+    {
+      icon: LockKeyOpen,
+      key: SecurityType.UNLOCK_TYPE,
+      title: t('Authenticate with password'),
+      url: '',
+      disabled: false
     }
   ], [noAccount, t]);
 
@@ -92,6 +136,14 @@ const Component: React.FC<Props> = (props: Props) => {
     activeModal(editUnlockTypeModalId);
   }, [activeModal]);
 
+  const onCloseAutoLockTimeModal = useCallback(() => {
+    inactiveModal(editAutoLockTimeModalId);
+  }, [inactiveModal]);
+
+  const onCloseUnlockTypeModal = useCallback(() => {
+    inactiveModal(editUnlockTypeModalId);
+  }, [inactiveModal]);
+
   const onClickItem = useCallback((item: SecurityItem) => {
     return () => {
       switch (item.key) {
@@ -106,6 +158,20 @@ const Component: React.FC<Props> = (props: Props) => {
       }
     };
   }, [navigate, onOpenAutoLockTimeModal, onOpenUnlockTypeModal]);
+
+  const onSelectTime = useCallback((item: AutoLockOption) => {
+    return () => {
+      inactiveModal(editAutoLockTimeModalId);
+      saveAutoLockTime(item.value).finally(noop);
+    };
+  }, [inactiveModal]);
+
+  const onSetUnlockType = useCallback((value: WalletUnlockType) => {
+    return () => {
+      inactiveModal(editAutoLockTimeModalId);
+      saveUnlockType(value).finally(noop);
+    };
+  }, [inactiveModal]);
 
   const onRenderItem = useCallback((item: SecurityItem) => {
     return (
@@ -141,7 +207,7 @@ const Component: React.FC<Props> = (props: Props) => {
   }, [onClickItem]);
 
   return (
-    <PageWrapper className={CN(className)}>
+    <PageWrapper className={CN(className, '-screen')}>
       <Layout.WithSubHeaderOnly
         backgroundImages={settingsScreensLayoutBackgroundImages}
         backgroundStyle={'secondary'}
@@ -174,18 +240,159 @@ const Component: React.FC<Props> = (props: Props) => {
             />
           </div>
         </div>
+        <SwModal
+          className={CN(className, '-auto-lock-modal', '-modal')}
+          id={editAutoLockTimeModalId}
+          onCancel={onCloseAutoLockTimeModal}
+          title={t('Auto lock')}
+        >
+          <div className='modal-body-container'>
+            {
+              autoLockOptions.map((item) => {
+                const _selected = timeAutoLock === item.value;
+
+                return (
+                  <SettingItem
+                    className={CN('__selection-item')}
+                    key={item.value}
+                    name={item.label}
+                    onPressItem={onSelectTime(item)}
+                    rightItem={
+                      _selected
+                        ? (
+                          <Icon
+                            className='__right-icon'
+                            iconColor='var(--icon-color)'
+                            phosphorIcon={CheckCircle}
+                            size='sm'
+                            type='phosphor'
+                            weight='fill'
+                          />
+                        )
+                        : null
+                    }
+                  />
+                );
+              })
+            }
+          </div>
+        </SwModal>
+        <SwModal
+          className={CN(className, '-authenticate-type-modal', '-modal')}
+          id={editUnlockTypeModalId}
+          onCancel={onCloseUnlockTypeModal}
+          title={t('Authenticate with password')}
+        >
+          <div className='modal-body-container'>
+            <SettingItem
+              className={CN('__selection-item')}
+              key={WalletUnlockType.ALWAYS_REQUIRED}
+              name={t('Always required')}
+              onPressItem={onSetUnlockType(WalletUnlockType.ALWAYS_REQUIRED)}
+              rightItem={
+                unlockType === WalletUnlockType.ALWAYS_REQUIRED
+                  ? (
+                    <Icon
+                      className='__right-icon'
+                      iconColor='var(--icon-color)'
+                      phosphorIcon={CheckCircle}
+                      size='sm'
+                      type='phosphor'
+                      weight='fill'
+                    />
+                  )
+                  : null
+              }
+            />
+            <SettingItem
+              className={CN('__selection-item')}
+              key={WalletUnlockType.WHEN_NEEDED}
+              name={t('When needed')}
+              onPressItem={onSetUnlockType(WalletUnlockType.WHEN_NEEDED)}
+              rightItem={
+                unlockType === WalletUnlockType.WHEN_NEEDED
+                  ? (
+                    <Icon
+                      className='__right-icon'
+                      iconColor='var(--icon-color)'
+                      phosphorIcon={CheckCircle}
+                      size='sm'
+                      type='phosphor'
+                      weight='fill'
+                    />
+                  )
+                  : null
+              }
+            />
+          </div>
+        </SwModal>
       </Layout.WithSubHeaderOnly>
     </PageWrapper>
   );
 };
 
-const SecurityList = styled(Component)<Props>(({ theme: { token } }: Props) => {
+const SecurityList = styled(Component)<Props>(({ theme: { extendToken, token } }: Props) => {
   return {
-    '.body-container': {
-      paddingTop: token.paddingXXS,
-      paddingRight: token.padding,
-      paddingLeft: token.padding,
-      paddingBottom: token.paddingLG
+    '&.-screen': {
+      '.body-container': {
+        paddingTop: token.paddingXXS,
+        paddingRight: token.padding,
+        paddingLeft: token.padding,
+        paddingBottom: token.paddingLG
+      }
+    },
+
+    '&.-modal': {
+      '.ant-sw-modal-body': {
+        paddingLeft: token.paddingXS,
+        paddingRight: token.paddingXS
+      },
+
+      '.modal-body-container': {
+        borderRadius: 20,
+        backgroundColor: extendToken.colorBgSecondary1,
+        padding: token.paddingXS
+      },
+
+      '.ant-setting-item': {
+        overflow: 'hidden',
+        backgroundColor: extendToken.colorBgSecondary1,
+        borderRadius: 40,
+
+        '.ant-web3-block-right-item': {
+          color: token.colorSuccess
+        },
+
+        '.ant-web3-block.ant-web3-block:hover': {
+          backgroundColor: token.colorBgSecondary
+        },
+
+        '.__left-icon': {
+          minWidth: 24,
+          height: 24,
+          borderRadius: 24,
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center'
+        },
+
+        '.ant-web3-block-left-item': {
+          paddingRight: token.paddingSM
+        },
+
+        '.ant-setting-item-name': {
+          color: token.colorTextDark2
+        },
+
+        '.ant-setting-item-content': {
+          paddingRight: token.paddingXS
+        },
+
+        '.__right-icon': {
+          minWidth: 40,
+          justifyContent: 'center'
+        }
+      }
     }
   };
 });
