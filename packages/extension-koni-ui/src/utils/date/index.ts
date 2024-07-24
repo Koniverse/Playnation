2d283c04fd3a30b8932300ed168f9573b2968c1e
anhnhu <anhnt@subwallet.app>
2024-07-24 16:59:38 +0700

    Update kick to airdrop leaderboard time

--- packages/extension-koni-ui/src/utils/date/index.ts
+++ packages/extension-koni-ui/src/utils/date/index.ts
@@ -66,7 +66,7 @@ export function calculateStartAndEnd (key: string) {
 
     case 'vara_playdrop': {
       const startEnv = process.env.VARA_PLAYDROP_START_DATE || '2024-07-10 10:00:00' as string;
-      const endEnv = process.env.VARA_PLAYDROP_END_DATE || '2024-07-24 10:00:00' as string;
+      const endEnv = process.env.VARA_PLAYDROP_END_DATE || '2024-07-31 10:00:00' as string;
       const startDate = new Date(startEnv);
       const endDate = new Date(endEnv);
 
