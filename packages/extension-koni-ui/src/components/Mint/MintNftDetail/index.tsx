5ee3b4deaf8a976d3990c5090abf2ab5e469120a
lw <linhtm@konistudio.xyz>
2024-11-21 10:16:06 +0700

    Open button Mint temporarily

--- packages/extension-koni-ui/src/components/Mint/MintNftDetail/index.tsx
+++ packages/extension-koni-ui/src/components/Mint/MintNftDetail/index.tsx
@@ -244,25 +244,27 @@ const Component: React.FC<Props> = (props: Props) => {
   }, []);
 
   const buttonType = (() => {
-    const now = Date.now();
-    const shouldCheck = airdropNftInfo?.start_mint && new Date(airdropNftInfo?.start_mint).getTime() < now;
-    const endCampaign = airdropNftInfo?.end && new Date(airdropNftInfo?.end).getTime() < now;
-    const eligibility = { currentProcess: IAirdropNftMintingProcess.ELIGIBLE };
+    return buttonTypeConst.MINT;
 
-    if (!shouldCheck && !endCampaign) {
-      return buttonTypeConst.COMING_SOON;
-    }
-
-    if (eligibility && eligibility.currentProcess) {
-      switch (eligibility.currentProcess) {
-        case IAirdropNftMintingProcess.END_CAMPAIGN:
-          return buttonTypeConst.END_CAMPAIGN;
-        default:
-          return buttonTypeConst.MINT;
-      }
-    } else {
-      return buttonTypeConst.MINT;
-    }
+    // const now = Date.now();
+    // const shouldCheck = airdropNftInfo?.start_mint && new Date(airdropNftInfo?.start_mint).getTime() < now;
+    // const endCampaign = airdropNftInfo?.end && new Date(airdropNftInfo?.end).getTime() < now;
+    // const eligibility = { currentProcess: IAirdropNftMintingProcess.ELIGIBLE };
+    //
+    // if (!shouldCheck && !endCampaign) {
+    //   return buttonTypeConst.COMING_SOON;
+    // }
+    //
+    // if (eligibility && eligibility.currentProcess) {
+    //   switch (eligibility.currentProcess) {
+    //     case IAirdropNftMintingProcess.END_CAMPAIGN:
+    //       return buttonTypeConst.END_CAMPAIGN;
+    //     default:
+    //       return buttonTypeConst.MINT;
+    //   }
+    // } else {
+    //   return buttonTypeConst.MINT;
+    // }
   })();
 
   const onMint = useCallback(async (address: string) => {
