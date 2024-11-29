61483fa642e86b8306d7fb20e22f9943bc9e8445
lw <linhtm@konistudio.xyz>
2024-11-29 14:25:43 +0700

    Enable debug mode temporarily

--- packages/extension-koni-ui/src/Popup/Home/Mint/index.tsx
+++ packages/extension-koni-ui/src/Popup/Home/Mint/index.tsx
@@ -34,11 +34,11 @@ const Component = ({ className }: Props): React.ReactElement => {
   }, []);
 
   const onClickLogo = useCallback(() => {
-    // setMintSuccess((prev) => !prev);
+    setMintSuccess((prev) => !prev);
   }, []);
 
   const onClickHeaderNameArea = useCallback(() => {
-    // setAlwaysShowMint((prev) => !prev);
+    setAlwaysShowMint((prev) => !prev);
   }, []);
 
   useEffect(() => {
