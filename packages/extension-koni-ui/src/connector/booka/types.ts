8824b5d38384daf9781255d728651b053e51c720
anhnhu <anhntk54@gmail.com>
2025-01-22 11:14:18 +0700

    [issue-270]: Update sdk account profile stats

--- packages/extension-koni-ui/src/connector/booka/types.ts
+++ packages/extension-koni-ui/src/connector/booka/types.ts
@@ -451,3 +451,21 @@ export interface NftMintingLog {
   notify?: boolean,
   extrinsicHash?: string,
 }
+
+export enum AccountIntegratedProfileStatus {
+  NOT_SYNCED = 'not_synced',
+  SYNCING = 'syncing',
+  SYNCED = 'synced',
+}
+
+export interface IntegratedProfileResult {
+  totalTransactions: number;
+  totalStakeVerioTransactions: number;
+  totalSwapPiperXTransactions: number;
+  offset: number;
+  totalBadgeNFTsOwned: number;
+  status: AccountIntegratedProfileStatus | null;
+  syncedAt?: Date | null;
+  telegramId?: number;
+  loginCount?: number;
+}
