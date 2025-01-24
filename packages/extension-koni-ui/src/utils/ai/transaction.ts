dc20c859a533aaa3dbc053f55bf5cd39bd2d2c9e
lw <linhtm@konistudio.xyz>
2025-01-24 11:59:31 +0700

    Update logic for transfer transaction

--- packages/extension-koni-ui/src/utils/ai/transaction.ts
+++ packages/extension-koni-ui/src/utils/ai/transaction.ts
@@ -11,39 +11,52 @@ export interface AiTransactionData {
 }
 
 const transformTransferData = (message: string): AiTransactionData => {
-  const transferPattern = /\n- Recipient Address:\s*(0x[0-9a-fA-F]{40})\n- Amount:\s*([\d.]+)\s*IP/;
-
-  const transferMatch = message.match(transferPattern);
-
-  if (!transferMatch) {
-    return {
-      type: 'unknown'
-    };
-  }
-
-  const recipientAddress = transferMatch[1];
-  const amount = transferMatch[2];
-
-  const data: Omit<RequestTransfer, 'from'> = {
-    value: BigN(amount).shiftedBy(18).toFixed(0),
-    to: recipientAddress,
-    networkKey: 'storyOdyssey_testnet',
-    tokenSlug: 'storyOdyssey_testnet-NATIVE-IP',
-    ignoreWarnings: true
-  };
-
-  return {
-    type: 'transfer',
-    data
-  };
-};
-
-const transformMintData = (message: string): AiTransactionData => {
-  const jsonMatch = message.match(/```json([\s\S]*?)```/)?.[1]?.trim();
-
   const defaultResult: AiTransactionData = {
     type: 'unknown'
   };
+
+  const jsonMatch = message.match(/```json([\s\S]*?)```/)?.[1]?.trim();
+
+  if (!jsonMatch) {
+    return defaultResult;
+  }
+
+  try {
+    const jsonObject = JSON.parse(jsonMatch) as {
+      recipient_address: string,
+      amount: number
+    };
+
+    const recipientAddress = jsonObject?.recipient_address;
+    const amount = jsonObject?.amount;
+
+    if (!recipientAddress || typeof amount === undefined) {
+      return defaultResult;
+    }
+
+    const data: Omit<RequestTransfer, 'from'> = {
+      value: BigN(amount).shiftedBy(18).toFixed(0),
+      to: recipientAddress,
+      networkKey: 'storyOdyssey_testnet',
+      tokenSlug: 'storyOdyssey_testnet-NATIVE-IP',
+      ignoreWarnings: true
+    };
+
+    return {
+      type: 'transfer',
+      data
+    };
+  } catch (e) {
+    return defaultResult;
+  }
+};
+
+const transformMintData = (message: string): AiTransactionData => {
+  const defaultResult: AiTransactionData = {
+    type: 'unknown'
+  };
+
+  const jsonMatch = message.match(/```json([\s\S]*?)```/)?.[1]?.trim();
 
   if (!jsonMatch) {
     return defaultResult;
@@ -71,15 +84,12 @@ const transformMintData = (message: string): AiTransactionData => {
       data
     };
   } catch (e) {
-    return {
-      type: 'unknown'
-    };
+    return defaultResult;
   }
 };
 
 export const transformAiMessageData = (message: string): AiTransactionData => {
-  const firstLine = message.split('\n')[0];
-  const isConfirmation = firstLine.startsWith('## IP') && firstLine.includes('confirmation');
+  const isConfirmation = message.split('\n').some((line) => line.startsWith('## IP') && line.includes('confirmation'));
 
   if (isConfirmation) {
     // is transfer
