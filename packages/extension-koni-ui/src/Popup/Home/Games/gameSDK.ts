340709f945ed776f0e13b8a38c715bea3e70fb84
Thiendekaco <thiendekaco@gmail.com>
2025-04-14 18:33:47 +0700

    [Issue-349] feat: Handle for user to continue playing event when event is stopped #2

--- packages/extension-koni-ui/src/Popup/Home/Games/gameSDK.ts
+++ packages/extension-koni-ui/src/Popup/Home/Games/gameSDK.ts
@@ -219,6 +219,8 @@ export class GameApp {
   }
 
   async onSubmitAction ({ gamePlayId, state }: {gamePlayId: string, state: GameState<any>}) {
+    // If the game is resumed, the game information will be fetched and used to update currentGamePlay,
+    // since currentGamePlay is only updated when a new game is created — and in a resumed game, this step is skipped because the gamePlay already exists.
     if (!this.apiSDK.currentGamePlay) {
       await this.apiSDK.getGamePlayById(Number.parseInt(gamePlayId));
     }
